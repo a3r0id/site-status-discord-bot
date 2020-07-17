d348b57cef4c72e56dd2686d2ff330f117c29d1d
aerobotpro <50676042+aerobotpro@users.noreply.github.com>
2020-07-16 21:14:19 -0700

    Update compile.bat

--- windows/compile.bat
+++ windows/compile.bat
@@ -2,6 +2,6 @@
 set /p begin=Hit ENTER to COMPILE SOURCE...
 pip3 install -r requirements.txt && cls && (echo pip3: Successfully Installed Discord.py!) || (pip install -r requirements.txt && cls && echo pip: Successfully Installed Discord.py!)
 cls
-pip3 install -r pyinstaller && cls || (pip install -r pyinstaller)
+pip3 install pyinstaller && cls || (pip install pyinstaller)
 pyinstaller -F bot.py -i discord-canary.ico
 

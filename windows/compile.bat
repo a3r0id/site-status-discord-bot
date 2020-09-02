b3c517d396ef49011c26f78890b78fd6da1f8ffd
aerobotpro <50676042+aerobotpro@users.noreply.github.com>
2020-09-02 03:36:26 -0700

    Delete compile.bat

--- windows/compile.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set /p begin=Hit ENTER to COMPILE SOURCE...
-pip3 install -r requirements.txt && cls && (echo pip3: Successfully Installed Discord.py!) || (pip install -r requirements.txt && cls && echo pip: Successfully Installed Discord.py!)
-cls
-pip3 install pyinstaller && cls || (pip install pyinstaller)
-pyinstaller -F bot.py -i discord-canary.ico
-
